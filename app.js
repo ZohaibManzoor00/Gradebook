277ede2d91f0fdd51c12fff3423f9a4808bf8bff
Charles Yarney <charlesyarney@gmail.com>
2022-04-12 10:48:56 -0400

    starting api

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const todosRouter = require('./routes/gradeBookRouter');
 
+const app = express();
+
+const PORT = 3031
+
+app.use(express.json())
+
+app.use(gradeBookRouter)
+
+app.listen(PORT, () => {
+    console.log(`App has been initialized on http://localhost:${PORT}`)
+})
---
+++ controller/gradeBookController.js
@@ -0,0 +1,5 @@
+const GradeBook = require("../models/GradeBook")
+
+module.exports = {
+    
+}
