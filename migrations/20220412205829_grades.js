ef157addd3597daf92686ef4a7d84c0771e5429e
Zohaib <zohaibmanzoor1700@gmail.com>
2022-04-14 11:52:07 -0700

    someStuff

--- migrations/20220412205829_grades.js
+++ migrations/20220412205829_grades.js
@@ -6,8 +6,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('grades', table => {
         // Prevent duplicate data
         table.primary(['student_id', 'assignment_id'])
-        table.string('student_id').notNullable()
-        table.string('assignment_id').notNullable()
+        table.integer('student_id').notNullable()
+        table.integer('assignment_id').notNullable()
         table.integer('grade').notNullable()
     })
 };
---
+++ seeds/04_seed_grades_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('grades').del()
+  const students = await knex('students').select('id')
+  const assignments = await knex('assignments').select('id')
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  
+  console.log(students)
+  console.log(assignments)
+  const grades = []
+  students.forEach(student => {
+    assignments.forEach(assignment => {
+      grades.push({
+        student_id:student.id,
+        assignment_id:assignment.id,
+        grade: getRandomInt(60, 100)
+      })
+    })
+  })
+  await knex('grades').insert(grades);
+};
